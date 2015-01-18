0c286820a03728e426ca13e304c0e52c0f4c4582
Matthew Collison <matthew@sixteenstudio.com>
2015-01-18 15:47:19 +0000

    Hand strength instantiation test added

--- tests/HandStrengthTest.php
+++ tests/HandStrengthTest.php
@@ -11,4 +11,27 @@
 
 namespace Sixteenstudio\Poker;
 
-class HandStrengthTest extends \PHPUnit_Framework_TestCase {}
+class HandStrengthTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @uses              \Sixteenstudio\Poker\Card
+     * @covers            \Sixteenstudio\Poker\HandStrength::__construct
+     * @covers            \Sixteenstudio\Poker\HandStrength::setCards
+     * @covers            \Sixteenstudio\Poker\HandStrength::getCards
+     * @covers            \Sixteenstudio\Poker\HandStrength::cardCount
+     */
+    public function testDeckInstantiatesWithCardsAndReturnsCorrectCardsAndCounts()
+    {
+        $cards = $this->getArrayOfCards1();
+
+        $handStrength = new HandStrength($cards);
+
+        foreach ($handStrength->getCards() as $key => $card) {
+            $this->assertEquals($card, $cards[$key]);
+        }
+
+        $this->assertEquals($handStrength->cardCount(), count($cards));
+    }
+
+
+}
